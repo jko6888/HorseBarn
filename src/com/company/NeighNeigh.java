05a1e253e024baec0db0de568f4aea165d54a9e6
BT_1N3_08@BKTECH.LOCAL <BT_1N3_08@BKTECH.LOCAL>
2018-10-24 12:52:59 -0400

    HorseBarn

---
+++ src/com/company/NeighNeigh.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class NeighNeigh implements Horse {
+    private String name;
+    private int weight;
+
+    public NeighNeigh (String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String getName () {
+        return this.name;
+    }
+
+    @Override
+    public int getWeight () {
+        return this.weight;
+    }
+}
